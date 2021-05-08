aefe9cb63ef8faee36bc766ecb6865b042333737
Michael Frieze <michaelfrieze@icloud.com>
2021-05-08 14:58:50 -0400

    using next link & router

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router';
+
 export default function EventPage() {
+  const router = useRouter();
+
+  console.log(router);
+
   return (
     <div>
       <h1>My Event</h1>
+      <h3>{router.query.slug}</h3>
+      <button onClick={() => router.push('/')}>Click</button>
     </div>
   );
 }
