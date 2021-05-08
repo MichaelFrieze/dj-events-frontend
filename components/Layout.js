e50a2ad5589619f8753c4a021b2c3fc64c7fcb6d
Michael Frieze <michaelfrieze@icloud.com>
2021-05-08 15:27:02 -0400

    added layout component | added head to layout

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+
+      {children}
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: 'DJ Events | Find the hottest parties',
+  description: 'Find the latest DJ and other musical events',
+  keywords: 'music, dj, edm, events',
+};
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
-import Link from 'next/link';
-import Head from 'next/head';
+import Layout from '../components/Layout';
 
 export default function HomePage() {
   return (
-    <div>
-      <Head>
-        <title>DJ Events</title>
-        <meta name="description" content="Welcome to DJ Events" />
-      </Head>
-
+    <Layout>
       <h1>Home</h1>
-      <Link href="/about">About</Link>
-    </div>
+    </Layout>
   );
 }
