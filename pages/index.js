1b19fc01ade11e13bbfb20ca67015896aa0b0a43
Michael Frieze <michaelfrieze@icloud.com>
2021-05-08 15:17:20 -0400

    added head tags to the index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function HomePage() {
   return (
     <div>
+      <Head>
+        <title>DJ Events</title>
+        <meta name="description" content="Welcome to DJ Events" />
+      </Head>
+
       <h1>Home</h1>
       <Link href="/about">About</Link>
     </div>
