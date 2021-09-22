940082fe73d50d1ee7db2e840cd77ca0a2830cdd
Michael Frieze <michaelfrieze@icloud.com>
2021-09-22 10:34:20 -0400

    Auth context

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css';
+import { AuthProvider } from '@/context/AuthContext';
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
+
+export default MyApp;
